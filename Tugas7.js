8bf189b5f4660bd4aa73bcfa75c8ebf32252cf38
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-15 11:53:34 +0700

    For array dan For OF
    For array dan For OF

---
+++ Tugas7.js
@@ -0,0 +1,14 @@
+function Tinggi() {
+    
+    var data = [150, 155, 160, 165, 170, 175, 180, 185, 195, 190];
+    
+    for (var i = data.length; i > 0; i--) {
+        console.log(data[i]);
+        
+    }
+    console.log(" ");
+    for (let x of data) {
+        console.log(x);
+    }
+}
+Tinggi();
